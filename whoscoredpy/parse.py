8232b0c8f3e12e98691a3a96e25a857af177592c
Keith Mascarenhas <keithxm23@gmail.com>
2016-01-26 02:07:46 +0000

    refactored code to process records

--- whoscoredpy/parse.py
+++ whoscoredpy/parse.py
@@ -1,7 +1,7 @@
 import pickle
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from schema import Game, Team, Player, TeamPlayer
+from schema import Game, Team, Player, TeamPlayer, Manager
 
 match_data = pickle.load(open('pickles/match_data.p', 'rb'))
 event_types = pickle.load(open('pickles/event_types.p', 'rb'))
@@ -11,29 +11,33 @@ engine = create_engine('sqlite:///whoscored.db', echo=True)
 Session = sessionmaker(bind=engine)
 session=Session()
 
+def process_record(klass, obj, key, session):
+    o = session.query(klass.id).filter(getattr(klass, key) == obj[key]).one_or_none()
+    if o is not None:
+        return o[0]
+    else:
+        o = klass(**obj)
+        session.add(o)
+        session.flush()
+        session.refresh(o)
+        return o.id
+
+def process_team(team_obj, session):
+    return process_record(Team, team_obj, 'ws_id', session)
+
+def process_manager(manager_obj, session):
+    return process_record(Manager, manager_obj, 'name', session)
+
 def process_player(player_obj, session):
     pass
 
-def process_team(team_obj, session):
-    team = session.query(Team.id).filter(Team.ws_id == team_obj['ws_id']).one_or_none()
-    if team is not None:
-        return team[0]
-    else:
-        team = Team(**team_obj)
-        session.add(team)
-        session.flush()
-        session.refresh(team)
-        return team.id
+home = match_data['home']
+away = match_data['away']
 
-def process_manager(manager_obj, session):
-    pass
+h_team_id = process_team({'ws_id': home['teamId'], 'name': home['name']}, session)
+a_team_id = process_team({'ws_id': away['teamId'], 'name': away['name']}, session)
 
-
-home_team_id = process_team({'ws_id': match_data['home']['teamId'],
-    'name': match_data['home']['name']}, session)
-away_team_id = process_team({'ws_id': match_data['away']['teamId'],
-    'name': match_data['away']['name']}, session)
-
-#manager_id = process_manager({}, session)
+h_manager_id = process_manager({'name':home['managerName']}, session)
+a_manager_id = process_manager({'name':away['managerName']}, session)
 
 session.commit()
